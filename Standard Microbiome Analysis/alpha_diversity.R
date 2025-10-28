768e3afd3e982e3c9e1dafdf3c5624597666a282
MaxWarriner <mwarriner@colgate.edu>
2025-10-28 17:25:47 -0400

    beta diversity figures

--- Standard Microbiome Analysis/alpha_diversity.R
+++ Standard Microbiome Analysis/alpha_diversity.R
@@ -1,11 +1,15 @@
 library(tidyverse)
 library(phyloseq)
+library(vegan)
 
+setwd("C:/Users/12697/Documents/MATH481_Max_Alta/Standard Microbiome Analysis")
 ps <- readRDS('microbiome.RDS')
 
 sam <- ps@sam_data
 
-food <- sam[,153:182]
+food <- sam[,154:183]
+
+#Microbiome diversity
 
 diversity <- estimate_richness(ps, measures = c('Shannon', 'Chao1'))
 
@@ -33,186 +37,204 @@ alpha_cors$Chao1_p[i] <- chao_cor$p.value
 }
 
 sig_cors <- alpha_cors |>
-  filter(Shannon_p <= 0.1 | Chao1_p <= 0.1)
+  filter(Shannon_p <= 0.1 | Chao1_p <= 0.1) |>
+  pull(nutrient)
 
+#sodium
 
-# Preliminary Violin Plots
+plot_sodium_shannon <- ggplot(data = food, aes(x = sodium_norm, y = Shannon)) + 
+  geom_point() + 
+  theme_bw() +
+  xlab('Mg sodium/week/1000 calories') + 
+  ylab('Shannon Diversity') + 
+  geom_smooth(method = c('lm'), se = F) + 
+  ggtitle(paste('Sodium vs. Shannon Diversity: r = ', round(cor(food$Shannon, food$sodium_norm), 3), ', p = ', round(alpha_cors$Shannon_p[5],3) , sep = ''))
 
-protein_violin <- ggplot(sam, aes(x = "", y = protein)) +
-  geom_violin(fill = "lightgrey", trim = FALSE) +
-  geom_boxplot(fill = "white", width = 0.25) +
-  get("theme_bw")() +
-  xlab("") +
-  ylab("Protein (g/week)") +
-  ggtitle("Protein") +
-  coord_flip() + 
-  theme(plot.title = element_text(hjust = 0.5))
-
-fat_violin <- ggplot(sam, aes(x = "", y = protein)) +
-  geom_violin(fill = "lightgrey", trim = FALSE) +
-  geom_boxplot(fill = "white", width = 0.25) +
-  get("theme_bw")() +
-  xlab("") +
-  ylab("Fat (g/week)") +
-  ggtitle("Fat") +
-  coord_flip() + 
-  theme(plot.title = element_text(hjust = 0.5))
-
-carbs_violin <- ggplot(sam, aes(x = "", y = carbs)) +
-  geom_violin(fill = "lightgrey", trim = FALSE) +
-  geom_boxplot(fill = "white", width = 0.25) +
-  get("theme_bw")() +
-  xlab("") +
-  ylab("Carbohydrates (g/week)") +
-  ggtitle("Carbohydrates") +
-  coord_flip() + 
-  theme(plot.title = element_text(hjust = 0.5))
-
-fiber_violin <- ggplot(sam, aes(x = "", y = fiber)) +
-  geom_violin(fill = "lightgrey", trim = FALSE) +
-  geom_boxplot(fill = "white", width = 0.25) +
-  get("theme_bw")() +
-  xlab("") +
-  ylab("Fiber (g/week)") +
-  ggtitle("Fiber") +
-  coord_flip() + 
-  theme(plot.title = element_text(hjust = 0.5))
+plot_sodium_chao <- ggplot(data = food, aes(x = sodium_norm, y = Chao1)) + 
+  geom_point() +
+  theme_bw() +
+  xlab('Mg sodium/week/1000 calories') + 
+  ylab('Chao1 Diversity') + 
+  geom_smooth(method = c('lm'), se = F) + 
+  ggtitle(paste('Sodium vs. Chao1 Diversity: r = ', round(cor(food$Chao1, food$sodium_norm), 3), ', p = ', round(alpha_cors$Chao1_p[5],3), sep = ''))
 
 library(patchwork)
+sodium <- plot_sodium_shannon + plot_sodium_chao
 
-violins <- protein_violin + fat_violin + carbs_violin + fiber_violin
+setwd("C:/Users/12697/Documents/MATH481_Max_Alta/Figures/Alpha Diversity")
+ggsave(sodium, filename = "sodium_alpha_diversity.png", width = 18, height = 6)
 
-ggsave(violins, filename = "violin_plots.png", width = 7, height = 5)
 
 
-# Scatter plots with alpha diversity
+#fruit portions
 
-protein <- ggplot(data = sam, aes(x = protein_norm, y = Shannon)) + 
+plot_fruit_portions_shannon <- ggplot(data = food, aes(x = fruit_portions_norm, y = Shannon)) + 
   geom_point() + 
   theme_bw() +
-  xlab('Protein (g/kg bodyweight)') +
-  ylab('Alpha Diversity (Shannon)') +
-  geom_smooth(method = c("lm"), se = F) + 
-  ggtitle(paste('Protein vs. Alpha Diversity: r = ', round(cor(sam$Shannon, sam$protein_norm), 3), sep = ''))
+  xlab('Fruit portions/week/1000 calories') + 
+  ylab('Shannon Diversity') + 
+  geom_smooth(method = c('lm'), se = F) + 
+  ggtitle(paste('Fruit Portions vs. Shannon Diversity: r = ', round(cor(food$Shannon, food$fruit_portions_norm), 3), ', p = ', round(alpha_cors$Shannon_p[9],3) , sep = ''))
+
+plot_fruit_portions_chao <- ggplot(data = food, aes(x = fruit_portions_norm, y = Chao1)) + 
+  geom_point() +
+  theme_bw() +
+  xlab('Fruit Portions/week/1000 calories') + 
+  ylab('Chao1 Diversity') + 
+  geom_smooth(method = c('lm'), se = F) + 
+  ggtitle(paste('Fruit Portions vs. Chao1 Diversity: r = ', round(cor(food$Chao1, food$fruit_portions_norm), 3), ', p = ', round(alpha_cors$Chao1_p[9],3), sep = ''))
+
+library(patchwork)
+fruit_portions <- plot_fruit_portions_shannon + plot_fruit_portions_chao
+
+ggsave(fruit_portions, filename = "fruit_portions_alpha_diversity.png", width = 18, height = 6)
 
 
-fat <- ggplot(data = sam, aes(x = fat_norm, y = Shannon)) + 
+#Plant Protein
+
+plot_plant_protein_shannon <- ggplot(data = food, aes(x = plant_protein_norm, y = Shannon)) + 
   geom_point() + 
   theme_bw() +
-  xlab('Fat (g/kg bodyweight)') +
-  ylab('Alpha Diversity (Shannon)') +
-  geom_smooth(method = c("lm"), se = F) + 
-  ggtitle(paste('Fat vs. Alpha Diversity: r = ', round(cor(sam$Shannon, sam$fat_norm), 3), sep = ''))
+  xlab('g Plant Protein/week/1000 calories') + 
+  ylab('Shannon Diversity') + 
+  geom_smooth(method = c('lm'), se = F) + 
+  ggtitle(paste('Plant Protein vs. Shannon Diversity: r = ', round(cor(food$Shannon, food$plant_protein_norm), 3), ', p = ', round(alpha_cors$Shannon_p[30],3) , sep = ''))
+
+plot_plant_protein_chao <- ggplot(data = food, aes(x = plant_protein_norm, y = Chao1)) + 
+  geom_point() +
+  theme_bw() +
+  xlab('g Plant Protein/week/1000 calories') + 
+  ylab('Chao1 Diversity') + 
+  geom_smooth(method = c('lm'), se = F) + 
+  ggtitle(paste('Plant Protein vs. Chao1 Diversity: r = ', round(cor(food$Chao1, food$plant_protein_norm), 3), ', p = ', round(alpha_cors$Chao1_p[30],3), sep = ''))
+
+library(patchwork)
+plant_protein <- plot_plant_protein_shannon + plot_plant_protein_chao
+
+ggsave(plant_protein, filename = "plant_protein_alpha_diversity.png", width = 18, height = 6)
+
+# Metabolite Diversity
+
+setwd("C:/Users/12697/Documents/MATH481_Max_Alta/Standard Microbiome Analysis")
+metabolites <- read_csv('metabolites_transposed.csv') |>
+  column_to_rownames('...1')
+
+sam <- ps@sam_data
+
+food <- sam[,154:183]
 
 
-carbs <- ggplot(data = sam, aes(x = carbs_norm, y = Shannon)) + 
+food$shannon <- diversity(metabolites, index = "shannon")
+food$simpson <- diversity(metabolites, index = "simpson")
+
+
+alpha_cors <- tibble(nutrient = colnames(food)[c(-31,-32)], 
+                     Shannon_r = rep(NA, 30),
+                     Shannon_p = rep(NA, 30),
+                     Simpson_r = rep(NA, 30), 
+                     Simpson_p = rep(NA, 30))
+
+
+#Test out nutrients against alpha diversity
+for(i in 1:30){
+  
+  shannon_cor <- cor.test(unlist(food[,i]),food$shannon)
+  alpha_cors$Shannon_r[i] <- shannon_cor$statistic
+  alpha_cors$Shannon_p[i] <- shannon_cor$p.value
+  
+  simpson_cor <- cor.test(unlist(food[,i]),food$simpson)
+  alpha_cors$Simpson_r[i] <- simpson_cor$statistic
+  alpha_cors$Simpson_p[i] <- simpson_cor$p.value
+  
+}
+
+
+sig_cors <- alpha_cors |>
+  filter(Shannon_p <= 0.1 | Simpson_p <= 0.1) |>
+  pull(nutrient)
+
+
+setwd("C:/Users/12697/Documents/MATH481_Max_Alta/Figures/Alpha Diversity/Metabolome")
+
+
+
+#Plant Protein
+
+plot_PUFA_shannon <- ggplot(data = food, aes(x = PUFA_norm, y = shannon)) + 
   geom_point() + 
   theme_bw() +
-  xlab('Carbohydrates (g/kg bodyweight)') +
-  ylab('Alpha Diversity (Shannon)') +
-  geom_smooth(method = c("lm"), se = F) + 
-  ggtitle(paste('Carbohydrates vs. Alpha Diversity: r = ', round(cor(sam$Shannon, sam$carbs_norm), 3), sep = ''))
+  xlab('g PUFA/week/1000 calories') + 
+  ylab('Shannon Diversity') + 
+  geom_smooth(method = c('lm'), se = F) + 
+  ggtitle(paste('PUFA vs. Shannon Diversity: r = ', round(cor(food$shannon, food$PUFA_norm), 3), ', p = ', round(alpha_cors$Shannon_p[13],3) , sep = ''))
 
-fiber <- ggplot(data = sam, aes(x = fiber_norm, y = Shannon)) + 
+plot_PUFA_simpson <- ggplot(data = food, aes(x = PUFA_norm, y = simpson)) + 
   geom_point() + 
   theme_bw() +
-  xlab('Fiber (g/kg bodyweight)') +
-  ylab('Alpha Diversity (Shannon)') +
-  geom_smooth(method = c("lm"), se = F) + 
-  ggtitle(paste('Fiber vs. Alpha Diversity: r = ', round(cor(sam$Shannon, sam$fiber_norm), 3), sep = ''))
+  xlab('g PUFA/week/1000 calories') + 
+  ylab('Simpson Diversity') + 
+  geom_smooth(method = c('lm'), se = F) + 
+  ggtitle(paste('PUFA vs. Simpson Diversity: r = ', round(cor(food$simpson, food$PUFA_norm), 3), ', p = ', round(alpha_cors$Simpson_p[13],3) , sep = ''))
 
-calories <- ggplot(data = sam, aes(x = calories_norm, y = Shannon)) + 
+
+library(patchwork)
+PUFA <- plot_PUFA_shannon + plot_PUFA_simpson
+
+ggsave(PUFA, filename = "PUFA_alpha_diversity.png", width = 18, height = 6)
+
+
+#Magnesium
+
+plot_magnesium_shannon <- ggplot(data = food, aes(x = magnesium_norm, y = shannon)) + 
   geom_point() + 
   theme_bw() +
-  xlab('calories/week/kg bodyweight') +
-  ylab('Alpha Diversity (Shannon)') +
-  geom_smooth(method = c("lm"), se = F) + 
-  ggtitle(paste('Calories vs. Alpha Diversity: r = ', round(cor(sam$Shannon, sam$calories_norm), 3), sep = ''))
+  xlab('Mg Magnesium/week/1000 calories') + 
+  ylab('Shannon Diversity') + 
+  geom_smooth(method = c('lm'), se = F) + 
+  ggtitle(paste('Magnesium vs. Shannon Diversity: r = ', round(cor(food$shannon, food$magnesium_norm), 3), ', p = ', round(alpha_cors$Shannon_p[25],3) , sep = ''))
 
-
-scatters <- protein + fat + carbs + fiber
-
-ggsave(scatters, filename = "scatters.png", width = 8, height = 6)
-
-
-mean(sam$calories)/7
-
-summary(lm(diversity ~ protein + fiber + carbs + fat + Age + sex + calories, data = sam))
-
-
-metab <- readxl::read_xlsx('metabolomics_data.xlsx')
-
-
-
-# food groups alpha diversity
-
-veg <- ggplot(data = sam, aes(x = vegetable_portions_norm, y = Shannon)) + 
+plot_magnesium_simpson <- ggplot(data = food, aes(x = magnesium_norm, y = simpson)) + 
   geom_point() + 
   theme_bw() +
-  xlab('Vegetable Portions/week') +
-  ylab('Alpha Diversity (Shannon)') +
-  geom_smooth(method = c("lm"), se = F) + 
-  ggtitle(paste('Vegetable Portions vs. Alpha Diversity: r = ', round(cor(sam$Shannon, sam$vegetable_portions_norm), 3), sep = ''))
+  xlab('Mg Magnesium/week/1000 calories') + 
+  ylab('Simpson Diversity') + 
+  geom_smooth(method = c('lm'), se = F) + 
+  ggtitle(paste('Magnesium vs. Simpson Diversity: r = ', round(cor(food$simpson, food$magnesium_norm), 3), ', p = ', round(alpha_cors$Simpson_p[25],3) , sep = ''))
 
 
-legumes <- ggplot(data = sam, aes(x = legume_portions_norm, y = Shannon)) + 
+library(patchwork)
+magnesium <- plot_magnesium_shannon + plot_magnesium_simpson
+
+ggsave(magnesium, filename = "magnesium_alpha_diversity.png", width = 18, height = 6)
+
+
+
+#fermented portions
+
+plot_fermented_portions_shannon <- ggplot(data = food, aes(x = fermented_portions_norm, y = shannon)) + 
   geom_point() + 
   theme_bw() +
-  xlab('Legume Portions/week') +
-  ylab('Alpha Diversity (Shannon)') +
-  geom_smooth(method = c("lm"), se = F) + 
-  ggtitle(paste('Legume Portions vs. Alpha Diversity: r = ', round(cor(sam$Shannon, sam$legume_portions_norm), 3), sep = ''))
+  xlab('Fermented Foods/week/1000 calories') + 
+  ylab('Shannon Diversity') + 
+  geom_smooth(method = c('lm'), se = F) + 
+  ggtitle(paste('Fermented Foods vs. Shannon Diversity: r = ', round(cor(food$shannon, food$fermented_portions_norm), 3), ', p = ', round(alpha_cors$Shannon_p[25],3) , sep = ''))
 
-
-grains <- ggplot(data = sam, aes(x = grain_portions_norm, y = Shannon)) + 
+plot_fermented_portions_simpson <- ggplot(data = food, aes(x = fermented_portions_norm, y = simpson)) + 
   geom_point() + 
   theme_bw() +
-  xlab('Grain Portions/week') +
-  ylab('Alpha Diversity (Shannon)') +
-  geom_smooth(method = c("lm"), se = F) + 
-  ggtitle(paste('Grain Portions vs. Alpha Diversity: r = ', round(cor(sam$Shannon, sam$grain_portions_norm), 3), sep = ''))
+  xlab('Fermented Foods/week/1000 calories') + 
+  ylab('Simpson Diversity') + 
+  geom_smooth(method = c('lm'), se = F) + 
+  ggtitle(paste('Fermented Foods vs. Simpson Diversity: r = ', round(cor(food$simpson, food$fermented_portions_norm), 3), ', p = ', round(alpha_cors$Simpson_p[25],3) , sep = ''))
 
 
-fruits <- ggplot(data = sam, aes(x = fruit_portions_norm, y = Shannon)) + 
-  geom_point() + 
-  theme_bw() +
-  xlab('Fruit Portions/week') +
-  ylab('Alpha Diversity (Shannon)') +
-  geom_smooth(method = c("lm"), se = F) + 
-  ggtitle(paste('Fruit Portions vs. Alpha Diversity: r = ', round(cor(sam$Shannon, sam$fruit_portions_norm), 3), sep = ''))
+library(patchwork)
+fermented_portions <- plot_fermented_portions_shannon + plot_fermented_portions_simpson
+
+ggsave(fermented_portions, filename = "fermented_portions_alpha_diversity.png", width = 18, height = 6)
 
 
-meats <- ggplot(data = sam, aes(x = meat_portions_norm, y = Shannon)) + 
-  geom_point() + 
-  theme_bw() +
-  xlab('Meat Portions/week') +
-  ylab('Alpha Diversity (Shannon)') +
-  geom_smooth(method = c("lm"), se = F) + 
-  ggtitle(paste('Meat Portions vs. Alpha Diversity: r = ', round(cor(sam$Shannon, sam$meat_portions_norm), 3), sep = ''))
 
 
-eggs_dairy <- ggplot(data = sam, aes(x = eggs_dairy_portions_norm, y = Shannon)) + 
-  geom_point() + 
-  theme_bw() +
-  xlab('Eggs & Dairy Portions/week') +
-  ylab('Alpha Diversity (Shannon)') +
-  geom_smooth(method = c("lm"), se = F) + 
-  ggtitle(paste('Egggs & Dairy Portions vs. Alpha Diversity: r = ', round(cor(sam$Shannon, sam$eggs_dairy_portions_norm), 3), sep = ''))
 
 
-proc_foods <- ggplot(data = sam, aes(x = processed_food_portions_norm, y = Shannon)) + 
-  geom_point() + 
-  theme_bw() +
-  xlab('Processed Food Portions/week') +
-  ylab('Alpha Diversity (Shannon)') +
-  geom_smooth(method = c("lm"), se = F) + 
-  ggtitle(paste('Processed Food Portions vs. Alpha Diversity: r = ', round(cor(sam$Shannon, sam$processed_food_portions_norm), 3), sep = ''))
-
-
-fermented_foods <- ggplot(data = sam, aes(x = fermented_portions_norm, y = Shannon)) + 
-  geom_point() + 
-  theme_bw() +
-  xlab('Fermented Foods Portions/week/kg bodyweight') +
-  ylab('Alpha Diversity (Shannon)') +
-  geom_smooth(method = c("lm"), se = F) + 
-  ggtitle(paste('Fermented Foods Portions vs. Alpha Diversity: r = ', round(cor(sam$Shannon, sam$fermented_portions_norm), 3), sep = ''))
